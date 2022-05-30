e353fc6fe94d84c2f4cb34a5d1eb17e00e220057
alvo254 <alvinkhamron@gmail.com>
2022-05-30 00:27:33 -0700

    print string

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Holberton School"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+#print("Holberton SchoolHolberton SchoolHolberton School")
+print(3 * str)
+print(str[:9])
